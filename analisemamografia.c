d9ef68f17a662252382b6b211e875d1c101f05a3
odran <odrand@users.noreply.github.com>
2015-12-15 10:39:56 -0200

    Done

--- analisemamografia.c
+++ analisemamografia.c
@@ -10,9 +10,9 @@
 
 #define TAMANHO 1024
 
-#define LIMIAR_INICIAL 30
-#define LIMIAR_INCREMENTO 5
-#define LIMIAR_POCENTAGEM_ACEITAVEL 0.3f
+#define LIMIAR_INICIAL 1
+#define LIMIAR_INCREMENTO 1
+//#define LIMIAR_POCENTAGEM_ACEITAVEL 0.35f
 
 typedef struct _PGMData {
     int row;
@@ -47,7 +47,6 @@ int pixel_mais_claro(int **matrix, int dim)
 
     return p;
 }
-
 
 void equalizar_imagem(PGMData *data)
 {
@@ -196,7 +195,7 @@ void writePGM(const char *filename, const PGMData *data)
     int i, j;
     int hi, lo;
 
-    int px_mais_claro = pixel_mais_claro(data->matrix,1024);
+//    int px_mais_claro = pixel_mais_claro(data->matrix,data->row);
 
     pgmFile = fopen(filename, "wb");
     if (pgmFile == NULL) {
@@ -254,15 +253,15 @@ void binarizarPGM(PGMData *data, int limiar)
 
 // encontra e retorna o valor de limiar aceito para a imagem, dentro dos parametros definidos
 // xc, yc = coordenadas do centro do tumor, tal com estao no arquivo fonte; raio = raio tal qual esta no arquivo
-int acharLimiar(PGMData *data, int xc, int yc, int raio)
+int acharLimiar(PGMData *data, int xc, int yc, int raio, float f)
 {
 	int limiar = LIMIAR_INICIAL,pixelValue;
 	int xi,yi;
 	int pixelsBrancos=0,pixelsTotal=0;
 	float porcentagem;
 
-	yc = TAMANHO - yc;
-
+//	yc = TAMANHO - yc;
+	if (f >= 0){
 	while(1)
 	{
 		for(xi = (-1)*raio; xi <= raio; xi++)
@@ -286,7 +285,7 @@ int acharLimiar(PGMData *data, int xc, int yc, int raio)
 
 		porcentagem = (float)pixelsBrancos/(float)pixelsTotal;
 
-		if(porcentagem > LIMIAR_POCENTAGEM_ACEITAVEL)
+		if(porcentagem > f)
 		{
 			limiar+=LIMIAR_INCREMENTO;
 			pixelsBrancos = 0;
@@ -297,7 +296,7 @@ int acharLimiar(PGMData *data, int xc, int yc, int raio)
 			break;
 		}
 	}
-
+	}
 	return limiar;
 }
 
@@ -312,29 +311,26 @@ void teste1(PGMData *data, int xc, int yc, int raio)
 
 	data->matrix[yc][xc] = 0;
 
-		int i,j;
-		for(i = -7; i < 7; i++)
+	int i,j;
+	for(i = -7; i < 7; i++)
+	{
+		for(j = -7; j < 7; j++)
 		{
-			 for(j = -7; j < 7; j++)
-		     {
-		    	 data->matrix[yc + i][xc + j] = 0;
-		     }
+			data->matrix[yc + i][xc + j] = 0;
 		}
-		for(i = -3; i < 3; i++)
+	}
+	for(i = -3; i < 3; i++)
+	{
+		for(j = -3; j < 3; j++)
 		{
-			for(j = -3; j < 3; j++)
-			{
-				data->matrix[yc + i][xc + j] = 255;
-			}
+			data->matrix[yc + i][xc + j] = 255;
 		}
+	}
 }
 
 void teste2(PGMData *data, int xc, int yc, int raio)
 {
-	int limiar = LIMIAR_INICIAL,pixelValue;
 	int xi,yi;
-	int pixelsBrancos=0,pixelsTotal=0;
-	float porcentagem;
 
 	yc = TAMANHO - yc;
 
@@ -344,6 +340,105 @@ void teste2(PGMData *data, int xc, int yc, int raio)
 
 		data->matrix[yi*-1 + yc][xi+xc] = data->matrix[yi*-1 + yc][xi+xc] ? 0 : 255;
 		data->matrix[yi + yc][xi+xc] = data->matrix[yi + yc][xi+xc] ? 0 : 255;
+	}
+}
+
+void teste3(PGMData *data, int xc, int yc, int raio)
+{
+	int xi,yi;
+
+//	yc = TAMANHO - yc;
+
+	for(xi = (-1)*raio; xi <= raio; xi++)
+	{
+		yi = round(sqrt(raio*raio - xi*xi));
+
+		data->matrix[yi*-1 + yc][xi+xc] = data->matrix[yi*-1 + yc][xi+xc] ? 0 : 255;
+		data->matrix[yi + yc][xi+xc] = data->matrix[yi + yc][xi+xc] ? 0 : 255;
+	}
+}
+
+//void limpar1(PGMData *data, int xc, int yc, int raio)
+//{
+//	int xi,yi;
+//
+////	yc = TAMANHO - yc;
+//
+//	for(xi = (-1)*raio; xi <= raio; xi++)
+//	{
+//		yi = round(sqrt(raio*raio - xi*xi));
+//
+//		data->matrix[yi*-1 + yc][xi+xc] = data->matrix[yi*-1 + yc][xi+xc] ? 0 : 255;
+//		data->matrix[yi + yc][xi+xc] = data->matrix[yi + yc][xi+xc] ? 0 : 255;
+//
+//		int i,j,dim=data->row;
+//
+//		int limiar = pixelMaisClaro * 0.6;
+//
+//		for(i = 0; i < dim; i++)
+//		{
+//			for(j = 0; j < dim; j++)
+//			{
+////				if (xi+xc < i and  )
+////				{
+////					data->matrix[j][i] = 0;
+////				}
+//			}
+//		}
+//	}
+//
+//
+//}
+
+void limpar2(PGMData *data, float f)
+{
+	if (f >= 0){
+	int xi,yi;
+
+//	yc = TAMANHO - yc;
+
+	int pixelMaisClaro = pixel_mais_claro(data->matrix,data->row);
+
+	int i,j,dim=data->row;
+
+	int limiar = pixelMaisClaro * f;
+
+	for(i = 0; i < dim; i++)
+	{
+		for(j = 0; j < dim; j++)
+		{
+			if (data->matrix[j][i] < limiar)
+			{
+				data->matrix[j][i] = 0;
+			}
+		}
+	}
+	}
+}
+
+void limpar3(PGMData *data, float f)
+{
+	if (f >= 0){
+	int xi,yi;
+
+//	yc = TAMANHO - yc;
+
+	int pixelPivo = data->matrix[data->row/2][data->row/2];
+
+	int i,j,dim=data->row;
+
+	int limiar = pixelPivo * f;
+
+	for(i = 0; i < dim; i++)
+	{
+		for(j = 0; j < dim; j++)
+		{
+			if (data->matrix[j][i] < limiar)
+			{
+				data->matrix[j][i] = 0;
+			}
+		}
+	}
 	}
 }
 
@@ -400,7 +495,7 @@ long double calculaResultado(Tripla *resultados, int n_iteracoes)
 
 long double calculaDimensaoFractal(PGMData *data, int x, int y, int raio){
     
-    y = TAMANHO - y;
+//    y = TAMANHO - y;
     
     Tripla resultados[100];
     
@@ -449,31 +544,71 @@ void grava_resultados(const char* nome_instancia, long double df)
     fclose(saida);
 }
 
+void cropRegiaoDeInteresse(PGMData *data, int xc, int yc, int raio)
+{
+	int xi,yi,i,j,dim=2*raio + 10;
+	int ** cropedMatriz;
+
+	yc = TAMANHO - yc;
+
+	cropedMatriz = allocate_dynamic_matrix(dim, dim);
+
+	int coordenadaInicialX = xc - raio - 5;
+	int coordenadaInicialY = yc - raio - 5;
+
+	for(i = 0; i < dim; i++)
+	{
+		for(j = 0; j < dim; j++)
+		{
+			cropedMatriz[j][i] = data->matrix[coordenadaInicialY+j][coordenadaInicialX+i];
+		}
+	}
+
+	deallocate_dynamic_matrix(data->matrix, data->row);
+
+	data->matrix = cropedMatriz;
+	data->col = data->row = dim;
+//	data->max_gray = pixel_mais_claro(data->matrix,dim);
+}
+
 
 int main(int argc, const char * argv[])
 {
 	PGMData matrix;
 	int limiar,xc,yc,raio;
+	float f1,f2,f3;
 
 	readPGM(argv[1], &matrix);
-
-	equalizar_imagem(&matrix);
 
 	xc = atoi(argv[3]);
 	yc = atoi(argv[4]);
 	raio = atoi(argv[5]);
 
-	//teste1(&matrix,xc,yc,raio);
+	f1 = atof(argv[6]);
+	f2 = atof(argv[7]);
+	f3 = atof(argv[8]);
 
-	limiar = acharLimiar(&matrix,xc,yc,raio);
-	binarizarPGM(&matrix, limiar);
+//	printf("%f %f %f\n", f1,f2,f3);
 
-	//teste2(&matrix,xc,yc,raio);
+//	cropRegiaoDeInteresse(&matrix,xc,yc,raio);
+//	equalizar_imagem(&matrix);
 
-    long double dimensao = calculaDimensaoFractal(&matrix, xc, yc, raio); // Válber usando. Ainda não está pronto
-    printf("Dimensão:%LF\n\n", dimensao);
-    
-    grava_resultados(argv[1], dimensao);
-    
+//	limpar2(&matrix, f2);
+//	limpar3(&matrix, f3);
+
+//	teste1(&matrix,xc,yc,raio);
+
+//	limiar = acharLimiar(&matrix,matrix.row/2,matrix.row/2,raio, f1);
+//	binarizarPGM(&matrix, limiar);
+
+	teste2(&matrix,xc,yc,raio);
+//	teste3(&matrix,matrix.row/2,matrix.row/2,raio);
+
+	long double dimensao;
+//	dimensao = calculaDimensaoFractal(&matrix, matrix.row/2, matrix.row/2, raio);
+//	printf("Dimensão:%LF\n\n", dimensao);
+//	grava_resultados(argv[2], dimensao);
+//	long double dimensao = calculaDimensaoFractal(&matrix, xc, yc, raio); // Válber usando. Ainda não está pronto
+
 	writePGM(argv[2], &matrix);
 }
